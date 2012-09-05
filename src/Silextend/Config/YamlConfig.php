143f3fe565892eb7c8923502c33be8c55c59e7e4
Mark Perkins <mark@allmarkedup.com>
2012-09-05 16:58:41 +0100

    Prevent choking on empty config files

--- src/Silextend/Config/YamlConfig.php
+++ src/Silextend/Config/YamlConfig.php
@@ -6,46 +6,47 @@ use Silex\Application;
 use Silex\ServiceProviderInterface;
 use Symfony\Component\Yaml\Yaml;
 
-class YamlConfig implements ServiceProviderInterface
+class YamlConfig implements ServiceProviderInterface 
 {
-    private $filenames;
+	private $filenames;
 
-    private $replacements = array();
+	private $replacements = array();
 
-    public function __construct($filenames, array $replacements = array())
-    {
+	public function __construct($filenames, array $replacements = array())
+	{
 		$this->filenames = is_array( $filenames ) ? $filenames : array($filenames);
        
-        if ($replacements) {
-            foreach ($replacements as $key => $value) {
-                $this->replacements['%'.$key.'%'] = $value;
-            }
-        }
-    }
-
-    public function register(Application $app)
-    {
-		$config = array();
-		foreach ( $this->filenames as $filename )
-		{
-	        if (!file_exists($filename)) {
-	            throw new \InvalidArgumentException(sprintf("The config file '%s' does not exist.", $filename));
-	        }
+		if ($replacements) {
+			foreach ($replacements as $key => $value) {
+				$this->replacements['%'.$key.'%'] = $value;
+			}
+		}
+	}
+	
+	public function register(Application $app)
+	{
+		$config = $config = array();
+		foreach ( $this->filenames as $filename ) {
 			
-			$parsed = Yaml::parse($filename);
+			if ( ! file_exists($filename) ) {
+				throw new \InvalidArgumentException(sprintf("The config file '%s' does not exist.", $filename));
+			}
 			
-	        if (null === $parsed) {
-	            throw new \InvalidArgumentException(sprintf("The config file '%s' appears to be invalid YAML.", $filename));
-	        }
-
-	        $config = $this->merge($config, $parsed);
+			if ( trim(file_get_contents($filename)) !== '' ) {
+				$parsed = Yaml::parse($filename);
+				if (null === $parsed) {
+					throw new \InvalidArgumentException(sprintf("The config file '%s' appears to be invalid YAML.", $filename));
+				}
+			}
+			
+			$config = $this->merge($config, $parsed);
 		}
 		
 		$replacedConfig = array();
-
-        foreach ($config as $name => $value) {
-            $replacedConfig[$name] = $this->doReplacements($value);
-        }
+		foreach ($config as $name => $value) {
+			$replacedConfig[$name] = $this->doReplacements($value);
+		}
+		
 		$app['config'] = $replacedConfig;
     }
 	
@@ -54,34 +55,29 @@ class YamlConfig implements ServiceProviderInterface
 		// nothing....
 	}
 	
-    private function doReplacements($value)
-    {
-        if (!$this->replacements) {
-            return $value;
-        }
+	private function doReplacements($value)
+	{
+		if (!$this->replacements) {
+			return $value;
+		}
 
-        if (is_array($value)) {
-            foreach ($value as $k => $v) {
-                $value[$k] = $this->doReplacements($v);
-            }
-
-            return $value;
-        }
-
-        return strtr($value, $this->replacements);
-    }
+		if (is_array($value)) {
+			foreach ($value as $k => $v) {
+				$value[$k] = $this->doReplacements($v);
+			}
+			return $value;
+		}
+		
+		return strtr($value, $this->replacements);
+	}
 	
 	protected function merge( array &$array1, array &$array2 )
 	{
 		$merged = $array1;
-		foreach ( $array2 as $key => &$value )
-		{
-			if ( is_array ( $value ) && isset ( $merged [$key] ) && is_array ( $merged [$key] ) )
-			{
+		foreach ( $array2 as $key => &$value ) {
+			if ( is_array ( $value ) && isset ( $merged [$key] ) && is_array ( $merged [$key] ) ) {
 				$merged [$key] = $this->merge ( $merged [$key], $value );
-			}
-			else
-			{
+			} else {
 				$merged [$key] = $value;
 			}
 		}
