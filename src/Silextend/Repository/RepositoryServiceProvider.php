8553e4792f369a0d909e737651960edf42b4cc1c
Mark Perkins <mark@allmarkedup.com>
2012-10-15 23:24:34 +0200

    Allow manual booting for console apps.

--- src/Silextend/Repository/RepositoryServiceProvider.php
+++ src/Silextend/Repository/RepositoryServiceProvider.php
@@ -7,23 +7,34 @@ use Silex\Application;
 
 class RepositoryServiceProvider implements ServiceProviderInterface
 {
+	protected $booted = false;
+	
 	public function register(Application $app)
 	{
-		$app->before(function() use ($app) {
-			foreach ($app['repository.repositories'] as $label => $class) {
-				$ident = 'db.' . $class::$dbName . '.' . $label;
-				$app[$ident] = $app->share(function($app) use ($class) {
-					return new $class($app['dbs'][$class::$dbName]);
-				});
-				if ( $class::$dbName === 'default' ) {
-					$app['db.' . $label] = $app[$ident];
-				}
-			}
-		});
+		if ( ! $this->booted ) {
+			$self = $this;
+			$app->before(function() use ($app,$self) {
+				$self->assignRepos($app);
+			});
+		}
 	}
 
 	public function boot(Application $app)
 	{
-		
+		$this->assignRepos($app);
+	}
+	
+	public function assignRepos($app)
+	{
+		foreach ($app['repository.repositories'] as $label => $class) {
+			$ident = 'db.' . $class::$dbName . '.' . $label;
+			$app[$ident] = $app->share(function($app) use ($class) {
+				return new $class($app['dbs'][$class::$dbName]);
+			});
+			if ( $class::$dbName === 'default' ) {
+				$app['db.' . $label] = $app[$ident];
+			}
+		}
+		$this->booted = true;
 	}
 }
